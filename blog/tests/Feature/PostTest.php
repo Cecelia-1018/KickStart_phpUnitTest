70a02270a415717a3588d19a946f60dafb906258
Cecelia-1018 <jieshi1018@gmail.com>
2022-09-20 16:42:40 +0800

    update

---
+++ blog/tests/Feature/PostTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostTest extends TestCase
+{
+   //Test
+    public function canCreateAPost()
+    {
+        $data = [
+          'title' => $this->faker->sentence,
+          'description' => $this->faker->paragraph 
+        ];
+
+        $response = $this->json('POST', '/api/v1/posts', $data);
+
+        $response->assertStatus(201)->assertJson(compact('data'));
+
+        $this->assertDatabaseHas('posts',[
+          'title' => $data['title'],
+          'description' => $data['description']
+        ]);
+    }
+}
--- blog/tests/TestCase.php
+++ blog/tests/TestCase.php
@@ -2,9 +2,29 @@
 
 namespace Tests;
 
+use Faker\Factory as Faker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, RefreshDatabase; //refresh database use for rest the database after each test
+
+    protected $faker;
+
+    //Sets up the tests
+    public function setUp(): void
+    {
+      parent::setUp();
+      $this->faker = Faker::create();
+      Artisan::call('migrate');  // runs the migration
+    }
+
+    //Rolls back migrations
+    public function tearDown(): void
+    {
+      Artisan::call('migrate:rollback');
+      parent::tearDown();
+    }
 }
